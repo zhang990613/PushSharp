32e179604464456376e7e59205878a06d7ea3bd8
zhang990613 <zhang990613@163.com>
2015-01-16 17:15:33 +0800

    Update GcmPushChannelSettings.cs
    add proxy to gcm setting
    public GcmPushChannelSettings(string senderAuthToken, ProxyInformation ProxyInfo)
                : this(senderAuthToken)
            {
                this.proxyInformation = ProxyInfo;
            }

--- PushSharp.Android/Gcm/GcmPushChannelSettings.cs
+++ PushSharp.Android/Gcm/GcmPushChannelSettings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,39 +6,44 @@ using PushSharp.Core;
 
 namespace PushSharp.Android
 {
-	public class GcmPushChannelSettings : IPushChannelSettings
-	{
-		private const string GCM_SEND_URL = "https://android.googleapis.com/gcm/send";
-
-		public GcmPushChannelSettings(string senderAuthToken)
-		{
-			this.SenderAuthToken = senderAuthToken;
-			this.GcmUrl = GCM_SEND_URL;
+    public class GcmPushChannelSettings : IPushChannelSettings
+    {
+        
+        private const string GCM_SEND_URL = "https://android.googleapis.com/gcm/send";
+        public GcmPushChannelSettings(string senderAuthToken, ProxyInformation ProxyInfo)
+            : this(senderAuthToken)
+        {
+            this.proxyInformation = ProxyInfo;
+        }
+        public GcmPushChannelSettings(string senderAuthToken)
+        {
+            this.SenderAuthToken = senderAuthToken;
+            this.GcmUrl = GCM_SEND_URL;
 
             this.ValidateServerCertificate = false;
         }
 
-		public GcmPushChannelSettings(string optionalSenderID, string senderAuthToken, string optionalApplicationIdPackageName)
-		{
-			this.SenderID = optionalSenderID;
-			this.SenderAuthToken = senderAuthToken;
-			this.ApplicationIdPackageName = optionalApplicationIdPackageName;
-			this.GcmUrl = GCM_SEND_URL;
+        public GcmPushChannelSettings(string optionalSenderID, string senderAuthToken, string optionalApplicationIdPackageName)
+        {
+            this.SenderID = optionalSenderID;
+            this.SenderAuthToken = senderAuthToken;
+            this.ApplicationIdPackageName = optionalApplicationIdPackageName;
+            this.GcmUrl = GCM_SEND_URL;
 
             this.ValidateServerCertificate = false;
         }
 
-		public string SenderID { get; private set; }
-		public string SenderAuthToken { get; private set; }
-		public string ApplicationIdPackageName { get; private set; }
+        public string SenderID { get; private set; }
+        public string SenderAuthToken { get; private set; }
+        public string ApplicationIdPackageName { get; private set; }
 
         public bool ValidateServerCertificate { get; set; }
+        public ProxyInformation proxyInformation = new ProxyInformation(false);
+        public string GcmUrl { get; set; }
 
-		public string GcmUrl { get; set; }
-
-		public void OverrideUrl(string url)
-		{
-			GcmUrl = url;
-		}
-	}
+        public void OverrideUrl(string url)
+        {
+            GcmUrl = url;
+        }
+    }
 }
